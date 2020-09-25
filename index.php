de7612de4185240f2c307898ef527487a1c3eb0f
unexpectedprojectz <alexjimenezcamacho76@gmail.com>
2020-09-25 16:21:45 +0200

    New

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    
+    <body>
+        <?php
+            echo "Hola alumnes";
+
+            echo "Alumnes i professors";
+
+            function funcion1() {
+                echo "Maria";
+            }
+        ?>
+    </body>
+    
+</html>
