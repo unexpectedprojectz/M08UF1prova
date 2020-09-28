609a635420ea3042906f73653228b5a7fb0ab46e
unexpectedprojectz <alexjimenezcamacho76@gmail.com>
2020-09-28 21:20:19 +0200

    AJC - S'ha canviat el text del primer echo.

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 
 <html>
-    
+
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
-    
+
     <body>
         <?php
-            echo "Hola alumnes";
+        echo "Hola alumnes!";
 
-            echo "Alumnes i professors";
+        echo "Alumnes i professors";
 
-            function funcion1() {
-                echo "Maria";
-            }
+        function funcion1() {
+            echo "Maria";
+        }
         ?>
     </body>
-    
+
 </html>
